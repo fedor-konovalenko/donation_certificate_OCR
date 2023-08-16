0b1873d3aa03ecbd5c1c1a435ddf3339dc55c836
sumkin <fdkonovalenko@gmail.com>
2023-08-17 00:22:27 +0300

    fixed docker problems, correction after review, add logging

--- app/src/app.py
+++ app/src/app.py
@@ -4,11 +4,20 @@ from fastapi.responses import FileResponse
 import uvicorn
 import argparse
 import os
+import logging
 from model import recognize_image
 
 app = FastAPI()
+#logging.basicConfig(filename="py_log.log",filemode="w", \
+                    #format="%(asctime)s %(levelname)s %(message)s")
 
-
+app_logger = logging.getLogger(__name__)
+app_logger.setLevel(logging.INFO)
+app_handler = logging.StreamHandler()#FileHandler(f"{__name__}.log", mode='w')
+app_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+app_handler.setFormatter(app_formatter)
+app_logger.addHandler(app_handler)    
+    
 @app.get("/health")
 def health():
     return {"status": "OK"}
@@ -28,17 +37,18 @@ def main():
 
 @app.post("/ocr")
 def process_request(file: UploadFile):
-    #  save file to the local folder
+    """save file to the local folder and send the image to the process function"""
     save_pth = os.path.join(os.path.dirname(__file__), "tmp", file.filename)
+    app_logger.info(f'processing file {save_pth}')
     with open(save_pth, "wb") as fid:
         fid.write(file.file.read())
-
-    # send the image to the process function
     global csv_path
     res, status, csv_path = recognize_image(save_pth)
-    if 'Изображение распознано' in status:      
+    if 'Изображение распознано' in status:
+        app_logger.info(f'processing status {status}')
         return {"filename": file.filename, "status": status, "info": res}
     else:
+        app_logger.warning(f'some problems {status}')
         return {"filename": file.filename, "status": status}        
 
     
@@ -49,9 +59,12 @@ if __name__ == "__main__":
     parser.add_argument("--host", default="0.0.0.0", type=str, dest="host")
     args = vars(parser.parse_args())
 
-    uvicorn.run(app, **args)
-
+uvicorn.run(app, **args)
 #if csv_path != '':
     #@app.get("/ocr")
     #def download_file():        
         #return FileResponse(path=csv_path, filename='download')
+        
+# def shutdown():
+#     os.kill(os.getpid(), signal.SIGTERM)
+#     return fastapi.Response(status_code=200, content='Server shutting down...')
--- app/src/model.py
+++ app/src/model.py
@@ -5,18 +5,28 @@ import re
 import numpy as np
 import datetime as dt
 import Levenshtein as lev
-import hashlib
+import logging
+
+#import hashlib
+
+m_logger = logging.getLogger(__name__)
+m_logger.setLevel(logging.DEBUG)
+handler_m = logging.StreamHandler()#FileHandler(f"{__name__}.log", mode='w')
+formatter_m = logging.Formatter("%(name)s %(asctime)s %(levelname)s %(message)s")
+handler_m.setFormatter(formatter_m)
+m_logger.addHandler(handler_m)
+
 
 from PIL import Image as PILImage
 from img2table.document import Image
-from pytesseract import Output
 import pytesseract
 
-# Предобработка изображений
-def pre(img):
+def pre(img: np.array) -> np.array:
+    """Предобработка изображений: обрезка, монохром, контраст"""
     try:
         height = int(img.shape[0])
     except:
+        m_logger.error(f'incorrect file')
         return('IT IS NOT AN IMAGE, DUDE')
     half = int(img.shape[0] / 2)
     cut = img[half:height, :]
@@ -28,74 +38,67 @@ def pre(img):
     return out
 
 
-# Поиск координат ячеек таблицы
-def search(pre_image_st, pre_image):
-    #print('ищу ячейки в -', path, '\n')
-    img = Image(pre_image_st, detect_rotation=False)
-    #print('контрольная сумма файла для поиска ячеек, \n')
-    #print(hashlib.sha1(open(path,'rb').read()).hexdigest())    
+def search(pre_image_st: str, pre_image: np.array) -> list:
+    """Поиск координат ячеек таблицы (построчно)"""
+    img = Image(pre_image_st, detect_rotation=False) 
     extracted_tables = img.extract_tables()
-    table_img = pre_image#(cv2.imread(path))
-    print('размер-', table_img.shape, '\n')
+    table_img = pre_image
     find_cell = []
     for table in extracted_tables:
         for row in table.content.values():
             for cell in row:
-                cv2.rectangle(table_img, (cell.bbox.x1, cell.bbox.y1), \
-                              (cell.bbox.x2, cell.bbox.y2), (255, 0, 0), 2)
-                rect = [cell.bbox.y1, cell.bbox.y2, cell.bbox.x1, cell.bbox.x2]
-                find_cell.append(rect)
-    print('найдено ячеек', len(find_cell))
+                rect = [cell.bbox.x1, cell.bbox.y1, cell.bbox.x2, cell.bbox.y2]
+                if (rect[3] - rect[1] > 10) and (rect[2] - rect[0] > 5) and (rect[3] < pre_image.shape[0]) and (rect[2] < pre_image.shape[1]):
+                    find_cell.append(rect)
+    m_logger.debug(f'найдено ячеек {len(find_cell)}')
     if len(find_cell) < 5:
+        m_logger.error(f'no table found')
         return 'PREPROCESSING ERROR TRY ANOTHER IMAGE'
     else:
         return find_cell
 
 
-# Распознавание числовых значений и дат
-def numbers(cells, pre_image):
+def numbers(cells: list, pre_image: np.array) -> list:
+    """распознавание чисел и дат"""
     cfg = r'--oem 3 --psm 7 outputbase digits'
     nums = []
-    img = pre_image#(cv2.imread(path))
-    #print('ищу цифры в -', path, '\n')
-    print('размер-', img.shape, '\n')
-    for place in cells:
-        print(place)
-        img_part = img[place[0]:place[1],place[2]:place[3]]
-        print(pytesseract.image_to_string(img_part, lang='rus', config=cfg))
+    img = pre_image
+    for place in cells:   
+        img_part = img[place[1]:place[3],place[0]:place[2]]
         nums.append(pytesseract.image_to_string(img_part, lang='rus', config=cfg))
     return nums
 
-
-# Распознавание всего остального
-def words(cells, pre_image):
+def words(cells: list, pre_image: np.array) -> list:
+    """Распознавание всего остального"""
     cfg = r'--oem 3 --psm 7'
     wds = []
-    img = pre_image#(cv2.imread(path))
+    img = pre_image
     for place in cells:
-        img_part = img[place[0]:place[1],place[2]:place[3]]
+        img_part = img[place[1]:place[3],place[0]:place[2]]
         wds.append(pytesseract.image_to_string(img_part, lang='rus', config=cfg))
     return wds
 
 
-# Объединение распознанных данных
-def join_data(n, w):
-    for i in range(len(n)):
-        if n[i] == '':
-            n[i] = w[i]
-    return n
+def join_data(numbers: list, words: list) -> list:
+    """Объединение распознанных данных"""
+    for i in range(len(numbers)):
+        if numbers[i] == '':
+            numbers[i] = words[i]
+    return numbers
 
 
-#Очистка выходных значений: оставляем только данные в формате "дата - 
-#тип донации - объем", удаляем пробельные символы и точки из конца строки
-
-def clean(list):
+def clean(tess_result: list) -> list:
+    """Очистка выходных значений: 
+    
+    оставляем только данные в формате "дата - тип донации - объем", 
+    удаляем пробельные символы и точки из конца строки
+    """
     stop_head = 0
-    for i in range(len(list)):
-        if re.search(r'\d{2}.\d{2}.\d{4}', list[i]):
+    for i in range(len(tess_result)):
+        if re.search(r'\d{2}.\d{2}.\d{4}', tess_result[i]):
             stop_head = i
             break
-    cut_head = list[stop_head:]
+    cut_head = tess_result[stop_head:]
     for j in range(len(cut_head)-1, 0, -1):
         if re.search(r'\d{2,}', cut_head[j]):
             stop_tail = j
@@ -111,6 +114,8 @@ def clean(list):
             clean_counter += 1
         clean_list[k] = re.sub(r'\s', '', clean_list[k])
         clean_list[k] = re.sub(r'[-, =]', '', clean_list[k])
+        if clean_list[k] == '':
+            clean_list[k] = 'unknown'
         if clean_list[k][-1] == '.':
             clean_list[k] = clean_list[k][:-1]
         if clean_list[k][0] == '.':
@@ -118,51 +123,49 @@ def clean(list):
     return clean_list
 
 
-# Удаление записей с датой, больше текущей
-def date_corr(d):
+def date_corr(d: dt.datetime) -> dt.datetime:
+    """Удаление записей с датой больше текущей"""
     td = dt.datetime.date(dt.datetime.today())
     if d > td:
         d = None
     return d
 
-def date_output(dt):
-    return dt.strftime("%Y-%m-%d")
-
-# Формирование таблицы из распознанных значений и сортировка их по дате
-def table(list):
+def table(final_list: list) -> pd.DataFrame:
+    """Формирование таблицы из распознанных значений и сортировка их по дате"""
     date = []
     volume = []
     don = []
     category = []
     error = []
     rec_counter = 0
-    for i in range(len(list)):
-        if re.fullmatch(r'\d{2}.\d{2}.\d{4}', list[i]):
+    for i in range(len(final_list)):
+        if re.fullmatch(r'\d{2}.\d{2}.\d{4}', final_list[i]):
             try:
-                d = dt.datetime.strptime(list[i], '%d.%m.%Y')
+                d = dt.datetime.strptime(final_list[i], '%d.%m.%Y')
                 d = dt.datetime.date(d)
                 rec_counter += 1
             except:
                 d = None
             date.append(d)
-        elif re.fullmatch(r'\d{3,4}', list[i]) and (50 <= int(list[i]) <= 750):
-            volume.append(int(list[i]))
-        elif re.search(r'[8, б, в, 6]', list[i].lower()) \
-             or re.search(r'[п, л, д, т, ф]', list[i].lower()):
+        elif re.fullmatch(r'\d{3,4}', final_list[i]) and \
+            (50 <= int(final_list[i]) <= 750):
+            volume.append(int(final_list[i]))
+        elif re.search(r'[8, б, в, 6]', final_list[i].lower()) \
+             or re.search(r'[п, л, д, т, ф]', final_list[i].lower()):
             don.append('Безвозмездно')
-            if lev.distance(list[i], 'кр/д') < 5 or \
-               lev.distance(list[i], 'ц/д') < 5 or \
-               re.search(r'[ц, к, р]', list[i].lower()):
+            if any([lev.distance(final_list[i], 'кр/д') < 5, \
+               lev.distance(final_list[i], 'ц/д') < 5, \
+               re.search(r'[ц, к, р]', final_list[i].lower())]):
                 category.append('Цельная кровь')
-            elif lev.distance(list[i], 'пл/д') < 5 or \
-                 lev.distance(list[i], 'п/ф') < 5 or \
-                 re.search(r'[п, л]', list[i].lower()): 
+            elif any([lev.distance(final_list[i], 'пл/д') < 5, \
+                 lev.distance(final_list[i], 'п/ф') < 5, \
+                 re.search(r'[п, л]', final_list[i].lower())]): 
                 category.append('Плазма')
-            elif lev.distance(list[i], 'т/ф') < 5 or \
-                 re.search(r'[т, ф]', list[i].lower()):
+            elif any([lev.distance(final_list[i], 'т/ф') < 5, \
+                 re.search(r'[т, ф]', final_list[i].lower())]):
                 category.append('Тромбоциты')
         else:
-            error.append(list[i])
+            error.append(final_list[i])
             volume.append('unknown')
             category.append('unknown')
             don.append('unknown')
@@ -179,14 +182,14 @@ def table(list):
         result = result.sort_values(by='Дата донации')
         result['Дата донации'] = result['Дата донации'].apply(date_corr)
         result = result.dropna(subset=["Дата донации"]).reset_index(drop=True)
-        result['Дата донации'] = result['Дата донации'].apply(date_output)
+        result['Дата донации'] = result['Дата донации'].\
+            apply(lambda x: x.strftime("%Y-%m-%d"))
         result = result.drop(['Объем, мл'], axis=1) #эххххх
         return result
 
 
 def recognize_image(file: str):
     IMAGE_PATH = file
-    print('предобработка для-', IMAGE_PATH, '\n')
     input_image = cv2.imread(IMAGE_PATH)
     preprocessed_image = pre(input_image)
     if type(preprocessed_image) == str:
@@ -194,11 +197,7 @@ def recognize_image(file: str):
         out_json = ''
         out_file = ''
         return out_json, status, out_file
-    #pre_path = 'tmp/' + re.sub(r'\D', '', IMAGE_PATH) + '.png'
-    #cv2.imwrite(pre_path, preprocessed_image)
     pre_img = cv2.imencode('.png', preprocessed_image)[1].tostring()
-    #print('контрольная сумма файла после предобработки, \n')
-    #print(hashlib.sha1(open(pre_path,'rb').read()).hexdigest())    
     cell = search(pre_img, preprocessed_image)
     if type(cell) == str:
         status = 'Ошибка распознавания. Попробуйте другое изображение'
